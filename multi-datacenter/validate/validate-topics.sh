aaad67e8b3b52d92d0e7e175ef713e53a72f57b0
Tim Stefany <40471052+ts170@users.noreply.github.com>
2022-01-28 15:55:43 +0100

    Delete multi-datacenter directory

--- multi-datacenter/validate/validate-topics.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-echo -e "Validate that the script at ${DIR}/../read-topics.sh completes"
-
-${DIR}/../read-topics.sh
