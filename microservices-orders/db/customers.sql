d33c8107400b0ff8c20271f932bba5c68fcebd57
Tim Stefany <40471052+ts170@users.noreply.github.com>
2022-01-28 15:55:31 +0100

    Delete microservices-orders directory

--- microservices-orders/db/customers.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS customers;
-CREATE TABLE customers (id INTEGER KEY NOT NULL, firstName VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), address VARCHAR(255), level VARCHAR(255));
-.separator '|'
-.import customers.table customers
--- microservices-orders/scripts/render-connector-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat <(eval "cat <<EOF
-$(<$INPUT_FILE)
-EOF
-") > $OUTPUT_FILE
