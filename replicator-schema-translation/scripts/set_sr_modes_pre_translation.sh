1d482ba026ada1d3c2f983d27748c675c338f52d
Tim Stefany <40471052+ts170@users.noreply.github.com>
2022-01-28 15:56:09 +0100

    Delete replicator-schema-translation directory

--- replicator-schema-translation/scripts/set_sr_modes_pre_translation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HEADER="Content-Type: application/json"
-
-echo -e "\nSetting srcSchemaregistry to READONLY mode:"
-curl -X PUT -H "${HEADER}" --data '{"mode": "READONLY"}' http://srcSchemaregistry:8085/mode
-
-echo -e "\nSetting destSchemaregistry to IMPORT mode:"
-curl -X PUT -H "${HEADER}" --data '{"mode": "IMPORT"}' http://destSchemaregistry:8086/mode
